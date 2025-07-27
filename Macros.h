f9a42f84dec493bc25c2c0d70f48446ff3b829e8
panagiotis dimitropoulos <162046043+Paltoudara@users.noreply.github.com>
2025-07-27 14:05:35 +0300

    Create Macros.h

---
+++ Macros.h
@@ -0,0 +1,44 @@
+
+#pragma once
+#include<exception>
+#include<string>
+#include<concepts>
+#define _NODISCARD [[nodiscard]]
+
+#define _CONSTEXPR constexpr
+#define _PANAGIOTIS_BEGIN namespace panagiotis{
+#define _PANAGIOTIS_END }
+
+
+_PANAGIOTIS_BEGIN
+class array_subscript_out_of_range :public std::exception
+{
+private:
+    std::string errorMessage; // To store the error message
+public:
+    //Constructor to initialize the error message
+    explicit array_subscript_out_of_range(const std::string& message)
+        : errorMessage(message) {
+    }
+
+    // Override the what() method
+    const char* what() const noexcept override {
+        return errorMessage.c_str();
+    }
+};
+class tried_to_access_an_empty_iterator_ :public std::exception
+{
+private:
+    std::string errorMessage; // To store the error message
+public:
+    //Constructor to initialize the error message
+    explicit tried_to_access_an_empty_iterator_(const std::string& message)
+        : errorMessage(message) {
+    }
+
+    // Override the what() method
+    const char* what() const noexcept override {
+        return errorMessage.c_str();
+    }
+};
+_PANAGIOTIS_END
